6e21cbdcf9586c9528ec91b3ef8e13dd6fd44bc2
Ridha Ahmad Firdaus <ridhaaf@gmail.com>
2024-03-28 15:33:03 +0700

    feat: add bottom navbar

--- app/src/main/java/com/ridhaaf/techtopia/MainActivity.kt
+++ app/src/main/java/com/ridhaaf/techtopia/MainActivity.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.ridhaaf.techtopia.core.presentation.components.DefaultBottomNavigation
 import com.ridhaaf.techtopia.core.presentation.routes.Routes
 import com.ridhaaf.techtopia.feature.presentation.home.HomeScreen
 import com.ridhaaf.techtopia.ui.theme.TechtopiaTheme
@@ -23,8 +27,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TechtopiaTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     App()
                 }
@@ -36,15 +39,33 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App() {
     val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+    val mainRoutes = listOf(
+        Routes.HOME,
+        Routes.WISHLIST,
+        Routes.ORDER_LIST,
+        Routes.PROFILE,
+    )
 
-    NavHost(
-        navController = navController,
-        startDestination = Routes.HOME,
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            if (currentDestination?.route in mainRoutes) {
+                DefaultBottomNavigation(currentDestination, navController)
+            }
+        },
     ) {
-        composable(Routes.HOME) {
-            HomeScreen(
-                navController = navController,
-            )
+        NavHost(
+            navController = navController,
+            startDestination = Routes.HOME,
+            modifier = Modifier.padding(it)
+        ) {
+            composable(Routes.HOME) {
+                HomeScreen(
+                    navController = navController,
+                )
+            }
         }
     }
 }
